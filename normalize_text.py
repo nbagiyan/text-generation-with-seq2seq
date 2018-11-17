92c35a8298a503d409d087717da94a10b867ebb3
Nerses Bagiyan <nbagiyan@yandex-team.ru>
2018-11-17 19:59:42 +0300

    change text preparation

--- normalize_text.py
+++ normalize_text.py
@@ -1,8 +1,10 @@
 import unicodedata
 import re
+import string
 
 def normalize_string(s):
     s = s.lower().strip()
     s = re.sub(r"([.!?])", r" \1", s)
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = ''.join([x for x in s if x not in string.punctuation])
     return s
