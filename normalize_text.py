fcb0c953110b28b3ffb22be4b71f934b3e5082be
Nerses Bagiyan <nbagiyan@yandex-team.ru>
2018-11-17 14:04:35 +0300

    add flag for val size

--- normalize_text.py
+++ normalize_text.py
@@ -1,15 +1,8 @@
 import unicodedata
 import re
 
-def unicode_to_ascii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )
-
-# Lowercase, trim, and remove non-letter characters
 def normalize_string(s):
-    s = unicode_to_ascii(s.lower().strip())
+    s = s.lower().strip()
     s = re.sub(r"([.!?])", r" \1", s)
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     return s
